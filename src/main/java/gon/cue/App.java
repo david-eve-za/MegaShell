c4a0814e38adcd6ec79a78ebf90f03fbbf25c7b8
david-eve-za <david.eve.za@gmail.com>
2018-07-05 15:40:52 +0000

    Adding all dependencies

--- src/main/java/gon/cue/App.java
+++ src/main/java/gon/cue/App.java
@@ -1,41 +1,60 @@
-/*
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package gon.cue;
 
+import java.io.File;
+import java.util.Arrays;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class App  {
+public class App {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(App.class, args);
-	}
-	
-	@Bean
+    public static void main(String[] args) {
+        initializeLog4j();
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             System.getProperties().list(System.out);//<- show all properties
             //System.exit(0);
 
-            //Arrays.stream(ctx.getBeanDefinitionNames()).forEach(item-> System.out.println(item));
+            Arrays.stream(ctx.getBeanDefinitionNames()).forEach(item-> System.out.println(item));
             //System.exit(0);
         };
+    }
+
+    private static void initializeLog4j() {
+        ConsoleAppender console = new ConsoleAppender(); // create appender
+        // configure the appender
+        String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+        console.setLayout(new PatternLayout(PATTERN));
+        console.setThreshold(Level.ALL);
+        console.activateOptions();
+        // add appender to any Logger (here is root)
+        Logger.getRootLogger().addAppender(console);
+
+        FileAppender fa = new FileAppender();
+        fa.setName("FileLogger");
+        fa.setFile("INFO.log");
+        fa.setLayout(new PatternLayout("%d %-5p [%c{1}] %m%n"));
+        fa.setThreshold(Level.ALL);
+        fa.setAppend(true);
+        fa.activateOptions();
+
+        // add appender to any Logger (here is root)
+        Logger.getRootLogger().addAppender(fa);
+        // repeat with all other desired appenders
     }
 }
